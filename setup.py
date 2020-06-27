882bc49812d5e69cde639100a3a6f0055d548a2e
Michal Povinsky <antihutka@gmail.com>
2020-06-27 04:33:25 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="sobutils-antihutka",
+    version="0.0.1",
+    author="Michal Povinsky",
+    author_email="antihutka@gmail.com",
+    description="Shared utils for bots",
+    long_description="Shared utils for bots",
+    long_description_content_type="text/markdown",
+    url="https://github.com/antihutka/sobutils",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
